cbb0e2cc54b8e1b8fd1c2f8a721f9f86b83d7a6c
Gloria0105 <raie2612@scs.ubbcluj.ro>
2020-10-25 16:24:16 +0200

    done

--- Lab2/src/models/Shared.java
+++ Lab2/src/models/Shared.java
@@ -1,67 +1,80 @@
 package models;
 
 import java.util.*;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Shared {
-		// Create a list shared by producer and consumer
-		// Size of list is 2.
-		Lock mutex = new ReentrantLock();
-		List<Integer> products=new ArrayList<>();
-		HashMap<Integer,Integer>listOfPairs;
-		public Shared(HashMap<Integer,Integer>listOfPairs){
-				this.listOfPairs=listOfPairs;
-		}
-		int capacity = 6;
 
-		// Function called by producer thread
-		public void produce() throws InterruptedException
-		{
+    Lock mutex = new ReentrantLock();
+    Queue<Integer> products = new LinkedList<>();
+    HashMap<Integer, Integer> listOfPairs;
+    List<Integer> sums = new ArrayList<>();
+    Integer sum = 0;
 
+    private final Condition bufferNotFull = mutex.newCondition();
+    private final Condition bufferNotEmpty = mutex.newCondition();
 
-								// producer thread waits while list
-								// is full
-								while (listOfPairs.size() == capacity) {
-										wait();
-								}
-								for( Map.Entry a :listOfPairs.entrySet()) {
-										Integer value = (Integer) a.getValue();
-										Integer key = (Integer) a.getKey();
-										Integer product = value * key;
-										mutex.lock();
-										products.add(product);
-										System.out.println("Producer produced-"
-														+ product);
-										mutex.unlock();
-								}
+    public Shared(HashMap<Integer, Integer> listOfPairs) {
+        this.listOfPairs = listOfPairs;
+    }
+
+    int capacity = 6;
 
 
-								// makes the working of program easier
-								// to  understand
+    // Function called by producer thread
+    public void produce() throws InterruptedException {
+
+        mutex.lock();
+        try {
+            if (products.size() == capacity) {
+                bufferNotEmpty.await();
+            }
+
+            for (Map.Entry<Integer, Integer> a : listOfPairs.entrySet()) {
+                Integer value = a.getValue();
+                Integer key = a.getKey();
+                Integer product = value * key;
+                products.offer(product);
+
+                System.out.println("Producer produced-"
+                        + product);
+                bufferNotFull.signalAll();
+
+            }
+        } finally {
+            mutex.unlock();
+
+        }
+        // makes the working of program easier
+        // to  understand
 
 
-		}
-		public void consume() throws InterruptedException {
+    }
+
+    public void consume() throws InterruptedException {
+        mutex.lock();
+        try {
+
+            while (products.size() == 0) {
+                bufferNotFull.await();
+            }
+            while (!products.isEmpty()) {
+                Integer value = products.poll();
+                if (value != null) {
+                    sum += value;
+                    sums.add(sum);
+                    System.out.println("Consumer consumed-"
+                            + sum);
+                    bufferNotEmpty.signalAll();
+                }
+            }
 
 
-								// consumer thread waits while list
-								// is empty
-								while (products.size() <= 1) {
-										wait();
-								}
-								// to retrive the ifrst job in the list
-								Integer sum = 0;
-									mutex.lock();
-								for(Integer product : products){
-										sum+=product;
-										System.out.println("Consumer consumed-"
-														+ sum);
-								}
+        } finally {
+            mutex.unlock();
+        }
 
-							mutex.unlock();
-
-
-				}
-
+    }
 }
